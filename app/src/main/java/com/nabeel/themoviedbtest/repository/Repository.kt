916ebdcf3cbaf351026d2445d72e77db827d4c0a
Nabeel Rangari <nabeelrangari8@gmail.com>
2020-06-08 15:23:13 +0530

    Repository Added

--- app/src/main/java/com/nabeel/themoviedbtest/repository/Repository.kt
+++ app/src/main/java/com/nabeel/themoviedbtest/repository/Repository.kt
@@ -1,6 +1,19 @@
 package com.nabeel.themoviedbtest.repository
 
 import android.content.Context
+import com.nabeel.themoviedbtest.data.network.NetworkService
+import com.nabeel.themoviedbtest.data.network.Networking
+import com.nabeel.themoviedbtest.util.AppConstant
 
 class Repository constructor(context: Context) {
+    private val api: NetworkService = Networking.getApiService(context, false)
+
+    suspend fun getUpcomingMovies(page: Int) = api.getUpcomingMovies(AppConstant.API_KEY, page)
+
+    suspend fun getPopularMovies(page: Int) = api.getPopularMovies(AppConstant.API_KEY, page)
+
+    suspend fun getTopRatedMovies(page: Int) = api.getTopRatedMovies(AppConstant.API_KEY, page)
+
+    suspend fun getMovieDetails(id: String) = api.getMovieDetails(id, AppConstant.API_KEY)
+
 }
---
+++ app/src/main/java/com/nabeel/themoviedbtest/util/AppConstant.kt
@@ -0,0 +1,9 @@
+package com.nabeel.themoviedbtest.util
+
+object AppConstant {
+    const val VIEW_TYPE_ITEM = 0
+    const val VIEW_TYPE_LOADING = 1
+
+    const val API_KEY: String = "89d19048206ea381fa73f3868d7280ed"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+}
